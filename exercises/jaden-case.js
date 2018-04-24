4967303b475727dee8522e57cc6dd8125d9ce496
Hermyo <yohann.hermeline@gmail.com>
2018-04-24 15:45:07 +0200

    ajout part 4

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -7,11 +7,23 @@
  * and return capitalize each words: "How are you ?" -> "How Are You ?"
  *
  */
-
+function jadenCase(string) {
+  let tab = string.split(' ');
+  for (let i = 0; i < tab.length; i++) {
+    let word = tab[i].split('');
+    word[0] = word[0].toUpperCase();
+    tab[i] = word.join('');
+  }
+  return tab.join(' ');
+}
 
 
 //* Begin of tests
 const assert = require('assert')
-
-assert.fail('You must write your own tests')
+assert.strictEqual(jadenCase('How are you ?'), 'How Are You ?')
+assert.strictEqual(jadenCase('Je code'), 'Je Code')
+assert.strictEqual(jadenCase('blabla'), 'Blabla')
+assert.strictEqual(jadenCase('je suis un wilder'), 'Je Suis Un Wilder')
+assert.strictEqual(jadenCase('tu es Sympa'), 'Tu Es Sympa')
+// assert.fail('You must write your own tests')
 // End of tests */
